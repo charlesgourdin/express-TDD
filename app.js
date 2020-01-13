4c33a65ef1590e9717cf8f52f582b9c1259915d3
Charles Gourdin <gourdin.charles@gmail.com>
2020-01-13 10:58:35 +0100

    Ajout de test sur post bookmarks err 422 ok

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // app.js
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
+const connection = require('./connection');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +12,14 @@ app.get('/', (req, res) => {
     res.json({
         message: "Hello World!"
     })
+})
+
+app.post('/bookmarks', (req, res) => {
+    const {url, title} = req.body
+    if (!url || !title){
+        return res.status(422).json({error: 'required field(s) missing'})
+    }
+    return res.sendStatus(201)
 })
 
 module.exports = app;
